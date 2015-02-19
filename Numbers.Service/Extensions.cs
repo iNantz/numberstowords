0522022ce2aca2cc99316b497d53a25a77d84b0e
inantz <renante.j.milan@gmail.com>
2015-02-19 10:02:24 +1000

    convert the function to string extension

--- Numbers.Service/Extensions.cs
+++ Numbers.Service/Extensions.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Numbers.IService;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -11,9 +14,44 @@ namespace Numbers.Service
     {
         private static Regex moDigits = new Regex(@"[^\d]");
 
+        /// <summary>
+        /// Get the all the digits
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         public static string GetDigits(this string str)
         {
             return moDigits.Replace(str, "");
+        }
+
+        /// <summary>
+        /// Convert Number to Words
+        /// </summary>
+        /// <param name="number">number to be converted to words</param>
+        /// <param name="currency">convert it to currency or not</param>
+        /// <returns></returns>
+        public static string ToWords(this string number, bool currency = false)
+        {
+            if (string.IsNullOrEmpty(number))
+                return number;
+
+            double ldNumber;
+            // configuration file for multilingual
+            var lsConfigFile = ConfigurationManager.AppSettings["jsonfile"] ?? "..\\..\\..\\JSon\\en-US.json";
+            var loNbrWord = Helpers.JSONStringToNumberWords(lsConfigFile) ?? new NumberWords();
+
+            // check if the number is valid
+            if (!double.TryParse(number, NumberStyles.Currency, loNbrWord.Culture, out ldNumber))
+                return "Invalid Number";
+
+            if (number.Contains(","))
+                number = number.Replace(",", "");
+
+            //  return the converted words
+            return string.Format("{0}{1} {2}",
+                                ldNumber < 0 ? string.Format("{0} ", loNbrWord.Negative.Trim()) : "",
+                                Helpers.ConvertWholeNumbersToWords(number, loNbrWord, currency),
+                                Helpers.ConvertDecimalsToWords(number, loNbrWord, currency)).Trim();
         }
     }
 }
--- Numbers.Service/Utils.cs
+++ Numbers.Service/Utils.cs
@@ -23,25 +23,8 @@ namespace Numbers.Service
         /// <returns>the number in words</returns>
         public void NumberToWords(string number, out string words, bool currency = false)
         {
-            double ldNumber;
-            var lsConfigFile = ConfigurationManager.AppSettings["jsonfile"] ?? "..\\..\\..\\JSon\\en-US.json";
-            var loNbrWord = Helpers.JSONStringToNumberWords(lsConfigFile) ?? new NumberWords();
-
-            // check if the number is valid
-            if (!double.TryParse(number, NumberStyles.Currency, loNbrWord.Culture, out ldNumber))
-            {
-                words =  "Invalid Number";
-                return;
-            }
-
-            if (number.Contains(","))
-                number = number.Replace(",", "");
-
-                //  return the converted words
-            words =  string.Format("{0}{1} {2}",
-                                ldNumber < 0 ? string.Format("{0} ", loNbrWord.Negative.Trim()) : "",
-                                Helpers.ConvertWholeNumbersToWords(number, loNbrWord, currency),
-                                Helpers.ConvertDecimalsToWords(number, loNbrWord, currency)).Trim();
+            // call the string extension to convert the number to words
+            words = number.ToWords(currency);
         }
 
         #endregion
